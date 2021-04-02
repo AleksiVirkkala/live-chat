06cae54338764f3d25fb77f22790f37830053a93
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-04-02 15:26:35 +0300

    feat: :sparkles: chatroom base

---
+++ src/views/Chatroom.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Chatroom</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Chatroom',
+  setup() {
+    //
+  },
+})
+</script>
+
+
+<style>
+
+</style>
