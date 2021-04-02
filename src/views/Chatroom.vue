402d1af3d3ecf596f54a60fb36a66e7a334ec31e
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-04-02 16:49:21 +0300

    feat: :sparkles: redirect user to login if they get logged out

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,15 +5,24 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, watch } from 'vue'
+import { useRouter } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
+import getUser from '@/composables/getUser'
 
 export default defineComponent({
   name: 'Chatroom',
   components: { Navbar },
 
   setup() {
-    //
+    const { user } = getUser()
+    const router = useRouter()
+
+    watch(user, newVal => {
+      if (newVal === null) {
+        router.push({ name: 'Welcome' })
+      }
+    })
   },
 })
 </script>
