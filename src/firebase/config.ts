2189d1394e628d2f84913d9effda20084103bf5a
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-04-02 18:28:08 +0300

    refactor: :truck: rename config file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-import firebaseConfig from './secret'
+import firebaseConfig from './.secret'
 
 // init firebase
 firebase.initializeApp(firebaseConfig)
