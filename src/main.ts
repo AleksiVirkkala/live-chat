2b35c3d7652fccace4c6dab66d0bb920276ddb42
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-04-02 16:24:17 +0300

    build: :sparkles: wait firebase connection before initializing Vue

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+// import firebase auth service
+import { projectAuth } from '@/firebase/config'
+
+let app: any
+
+projectAuth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App)
+      .use(router)
+      .mount('#app')
+  }
+})
