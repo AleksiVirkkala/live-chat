58d69a9afdc576b899a2c134d5cb4c51b0609dda
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-04-02 13:26:44 +0300

    feat: :sparkles: implement project base

---
+++ src/views/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Welcome',
+})
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
