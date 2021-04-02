219c9a1b504ca3d75edb77b05458954ccc456a25
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-04-02 15:49:51 +0300

    feat(firebase): :sparkles: logout user from navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,17 +4,28 @@
       <p>Hey there... display name here</p>
       <p class="email">Currently logged in as... email</p>
     </div>
-    <button>Logout</button>
+    <button @click="handleClick">Logout</button>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import useLogout from '@/composables/useLogout'
 
 export default defineComponent({
   name: 'Navbar',
   setup() {
-    //
+    const { error, logout } = useLogout()
+
+    const handleClick = async () => {
+      await logout()
+
+      if (!error.value) {
+        console.log('User logged out')
+      }
+    }
+
+    return { logout, handleClick }
   },
 })
 </script>
