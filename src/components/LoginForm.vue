766f84c2c5c40ee10a7e1519c0787adc94136164
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-04-02 14:17:44 +0300

    feat: :sparkles: implement login and signup components ui

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input v-model="email" type="email" required placeholder="email">
+    <input v-model="password" type="password" required placeholder="password">
+    <button>Log in</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'LoginForm',
+  setup() {
+    // refs
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = () => {
+      console.log(email.value, password.value)
+    }
+
+    return { email, password, handleSubmit }
+  },
+})
+</script>
+
+<style></style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,20 +1,43 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <SignupForm />
+    <LoginForm />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import SignupForm from '@/components/SignupForm.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 export default defineComponent({
   name: 'Welcome',
+  components: { SignupForm, LoginForm },
 })
 </script>
 
 <style>
-.welcome {
-  text-align: center;
-  padding: 20px 0;
-}
+  .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
 </style>
