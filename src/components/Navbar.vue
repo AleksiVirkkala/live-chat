60c97728bbd529b4a7162e2ef650438742dd943a
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-04-02 15:38:38 +0300

    feat: :sparkles: implement navbar ui

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there... display name here</p>
+      <p class="email">Currently logged in as... email</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Navbar',
+  setup() {
+    //
+  },
+})
+</script>
+
+<style>
+  nav {
+    padding: 20px;
+    border-bottom: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  nav p {
+    margin: 2px auto;
+    font-size: 16px;
+    color: #444;
+  }
+  nav p.email {
+    font-size: 14px;
+    color: #999;
+  }
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,12 +1,17 @@
 <template>
-  <p>Chatroom</p>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default defineComponent({
   name: 'Chatroom',
+  components: { Navbar },
+
   setup() {
     //
   },
